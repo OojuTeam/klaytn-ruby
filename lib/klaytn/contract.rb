ca86a21ec2f3e00313f659ae1a522760ecf46ee5
Ryan Kulp <ryanckulp@gmail.com>
2022-03-05 21:41:37 +0900

    begin contract interaction

--- lib/klaytn/contract.rb
+++ lib/klaytn/contract.rb
@@ -2,9 +2,10 @@ module Klaytn
   class Contract < Client
     MISSING_ACCOUNT_WALLET_MSG = 'Please provide a KAS Account wallet to pay for transactions.' # created via KAS > Service > Wallet > Account Pool > Create Account Pool > Create Account
     MISSING_ACCOUNT_POOL_KRN = 'Please provide a KAS Account Pool KRN id to finish linking your KAS Wallet (ex: krn:XXXX:wallet:yyyy...).' # KAS > Service > Wallet > Account Pool > KRN
+    FUNCTION_NOT_FOUND = 'Function with definition XXX not found.'
     BASE_URL = 'https://wallet-api.klaytnapi.com/v2/tx/contract'.freeze
 
-    attr_reader :kas_account_wallet_address, :kas_account_pool_krn, :encoder
+    attr_reader :kas_account_wallet_address, :kas_account_pool_krn, :abi, :encoder
 
     def initialize(opts)
       raise MISSING_ACCOUNT_WALLET_MSG if opts[:kas_account_wallet_address].blank?
@@ -12,6 +13,8 @@ module Klaytn
 
       @kas_account_wallet_address = opts[:kas_account_wallet_address]
       @kas_account_pool_krn = opts[:kas_account_pool_krn]
+      @abi = opts[:abi]
+
       @encoder = Encoder.new
 
       super
@@ -26,6 +29,35 @@ module Klaytn
 
       resp = HTTParty.post(BASE_URL + '/deploy', body: body.to_json, headers: headers, basic_auth: basic_auth)
       JSON.parse(resp.body)
+    end
+
+    ## DEPRECATE: definition ex: 'addAddressToWhitelist(address)' - string literal Solidity function definition
+    # definition ex: 'addAddressToWhitelist' - string literal Solidity function definition, without parameters/parens
+    # inputs ex: [ OpenStruct.new({ "internalType": "address", "name": "addr", "type": "address" }) ] - array of dot-notation object from ABI
+    # params ex: ['0x0xxxx'] - array of arguments accepted by function
+
+    def invoke_function(definition, inputs, params, submit: true)
+      raise MISSING_CONTRACT_MSG if contract_address.blank?
+
+      found_function = abi.filter {|input| input[:name] == definition }.first
+      raise FUNCTION_NOT_FOUND.gsub('XXX', definition) if found_function.nil?
+
+      built_defintion = "#{definition}(#{found_function[:inputs].map {|i| i[:type]}.join(',')})"
+      built_inputs = found_function[:inputs].map { |i| OpenStruct.new(i) }
+
+      body = function_body_builder(built_defintion, built_inputs, params)
+      resp = HTTParty.post(BASE_URL + '/execute', body: body.to_json, headers: headers, basic_auth: basic_auth)
+      JSON.parse(resp.body)
+    end
+
+    def function_body_builder(definition, inputs, params, submit)
+      {
+        from: kas_account_wallet_address,
+        to: contract_address,
+        input: encoder.encode_function(definition, inputs, params),
+        gas: 8500000, # 'gas' parameter not required, possibly better to exclude
+        submit: submit
+      }
     end
 
     def headers
