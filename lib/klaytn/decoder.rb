58f41175012b1a153852aafeec2597c294cb6bd6
Ryan Kulp <ryanckulp@gmail.com>
2022-03-06 01:53:12 +0900

    handle decoding without Ethereum dependency

--- lib/klaytn/decoder.rb
+++ lib/klaytn/decoder.rb
@@ -1,7 +1,15 @@
 module Klaytn
   class Decoder
-    def decode_uint(encoded_uint) # ex: 0x4563918244f40000 => 5000000000000000000 (5 KLAY)
-      Ethereum::Decoder.new.decode_uint(encoded_uint)
+    def decode_uint(value, subtype = "256", start = 0)
+      trim(value, start, bitsize(subtype)).hex
+    end
+
+    def trim(value, start, bitsize = 256)
+      value[start+63-(bitsize/4-1)..start+63]
+    end
+
+    def bitsize(subtype, default = 256)
+      subtype.present? ? subtype.to_i : default
     end
   end
 end
