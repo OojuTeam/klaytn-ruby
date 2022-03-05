0fa6633fea80e6431bbed36233d605260d78f63a
Ryan Kulp <ryanckulp@gmail.com>
2022-03-05 21:08:46 +0900

    fix transaction "submit" truthiness

--- lib/klaytn/transaction.rb
+++ lib/klaytn/transaction.rb
@@ -16,13 +16,14 @@ module Klaytn
     end
 
     def send(address, amount_in_peb, opts = {})
+      should_submit = opts[:submit] == false ? false : true
       body = {
         from: kas_account_wallet_address,
         to: address,
         value: encoder.encode_uint(amount_in_peb), # '0x0' = 0 KLAY, '0x4563918244f40000' = 5 KLAY
         memo: opts[:memo], # can be a text note; viewable on Klaytn Scope!
         gas: opts[:gas] || 200000, # not required, default is 1,000,000 if excluded
-        submit: opts[:submit] || true # create real transaction by default
+        submit: should_submit # create real transaction by default
       }
 
       resp = HTTParty.post(BASE_URL + '/value', body: body.to_json, headers: headers, basic_auth: basic_auth)
